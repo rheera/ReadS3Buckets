8c0c6319f02403d574135563adfbc7d3dc0ce967
Raman Heera <rheera2@uwo.ca>
2020-05-05 22:05:10 -0400

    Function to print bucket names

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 
 # sets up an S3 service object we can interface with (call methods on and work with)
@@ -8,10 +7,11 @@ s3 = boto3.resource('s3')
 def lambda_handler(event, context):
     # make an empty list
     bucket_list = []
+    # for loop to print the bucket names and add them to our list
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+        bucket_list.append(bucket.name)
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": bucket_list,
     }
